411328bdd23cf93a6e683ccc0870a869ea55a05f
bhavana26 <61701149+bhavana26@users.noreply.github.com>
2023-02-05 19:05:21 -0500

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ import sqlite3
 from flask import Flask, request, g, render_template, send_file
 
 DATABASE = '/var/www/html/flaskapp/example.db'
-#DATABASE = 'C:/Users/karan/OneDrive/Documents/AWS folder cloud/example.db'
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
